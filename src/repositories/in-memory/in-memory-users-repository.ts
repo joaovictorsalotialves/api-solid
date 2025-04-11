77453a26f889618c4c76f11fa4dcb64c0b7a71dd
João Victor <joaovictorsalotialves12@gmail.com>
2025-04-11 11:00:22 -0300

    Caso de uso de check-in

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,4 +1,6 @@
 import type { Prisma, User } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
 import type { UsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements UsersRepository {
@@ -26,7 +28,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
@@ -38,14 +40,3 @@ export class InMemoryUsersRepository implements UsersRepository {
     return user
   }
 }
-
-// {
-//       async findByEmail(email) {
-//         return null
-//       },
-//       async create(data) {
-//         return {
-//
-//         }
-//       },
-//     }
