d10582c20bff44d43d25cc448250411596fa08e8
João Victor <joaovictorsalotialves12@gmail.com>
2025-04-12 00:36:53 -0300

    Caso de uso de validar de check-in

---
+++ src/use-cases/validate-check-in.ts
@@ -0,0 +1,38 @@
+import type { CheckIn } from '@prisma/client'
+
+import type { CheckInsRepository } from '@/repositories/check-ins-repository'
+import type { GymsRepository } from '@/repositories/gyms-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
+import { MaxDistanceError } from './errors/max-distance-error'
+import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-erros'
+
+interface ValidateCheckInUseCaseRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.save(checkIn)
+
+    return {
+      checkIn,
+    }
+  }
+}
