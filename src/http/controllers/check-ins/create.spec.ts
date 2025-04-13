4115fa41a48c18637e8916db34d7441c1aaa7493
João Victor <joaovictorsalotialves12@gmail.com>
2025-04-13 14:38:43 -0300

    Testes E2E de rotas de check-ins

--- src/http/controllers/check-ins/create.spec.ts
+++ src/http/controllers/check-ins/create.spec.ts
@@ -2,8 +2,9 @@ import request from 'supertest'
 import { app } from '@/app'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
 
-describe('Create Gym (e2e)', () => {
+describe('Create Check-in (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,16 +13,23 @@ describe('Create Gym (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to create a gym', async () => {
+  it('should be able to create a check-in', async () => {
     const { token } = await createAndAuthenticateUser(app)
 
-    const response = await request(app.server)
-      .post('/gyms')
-      .set('Authorization', `Bearer ${token}`)
-      .send({
+    const gym = await prisma.gym.create({
+      data: {
         title: 'JavaScript Gym',
         description: 'Some description.',
         phone: '11999999999',
+        latitude: -21.5566962,
+        longitude: -46.8103111,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
         latitude: -21.5566962,
         longitude: -46.8103111,
       })
